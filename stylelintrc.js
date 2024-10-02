8dacae1c752d182ddac63d852fc19d8488b00961
Pavel Cherdantsev <pavel.cherdantsev@softswiss.com>
2024-10-02 17:21:04 +0400

    media queries for parent before nested selectors

--- stylelintrc.js
+++ stylelintrc.js
@@ -6,6 +6,24 @@ module.exports = {
     'scss/double-slash-comment-whitespace-inside': 'always',
     'scss/no-global-function-names': null,
     'property-no-vendor-prefix': null,
+    'order/order': [
+      [
+        'dollar-variables',
+        'custom-properties',
+        'at-rules',
+        'declarations',
+        {
+          type: 'at-rule',
+          name: 'media',
+        },
+        {
+          type: 'at-rule',
+          name: 'supports',
+        },
+        'rules',
+      ],
+      { severity: 'warning' },
+    ],
   },
   ignoreFiles: ['**/normalize.css'],
 };
